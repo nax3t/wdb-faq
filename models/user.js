46869b47d31d354a1670a554430a34c119c78efb
Darrell Pawson <dpawson905@gmail.com>
2018-11-14 20:36:12 -0500

    site auth working

--- models/user.js
+++ models/user.js
@@ -1,9 +1,34 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	isAdmin: { type: Boolean, default: false }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  isVerified: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+}, {
+  timestamps: true
 });
 
 UserSchema.plugin(passportLocalMongoose);
