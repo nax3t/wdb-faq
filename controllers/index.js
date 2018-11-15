5124b8b30a72314aee92232119a576abea55829e
Darrell Pawson <dpawson905@gmail.com>
2018-11-14 20:43:47 -0500

    authentication working

--- controllers/index.js
+++ controllers/index.js
@@ -6,34 +6,4 @@ module.exports = {
 	getHome(req, res, next) {
 		res.redirect('/solutions')
 	},
-	// GET Register
-	getRegister(req, res, next) {
-		res.render('register');
-	},
-	// POST Register
-	async postRegister(req, res, next) {
-		console.log('registering user');
-		let newUser = await User.register(new User({username: req.body.username}), req.body.password);
-	  // Check for isAdmin
-		req.body.isAdmin === process.env.IS_ADMIN_CODE ? newUser.isAdmin = true : newUser.isAdmin = false;
-		newUser.save();
-		console.log('user registered!');
-		res.redirect('/');
-	},
-	// GET Login
-	getLogin(req, res, next) {
-		res.render('login');
-	},
-	// POST Login
-	async postLogin(req, res, next) {
-		await passport.authenticate('local', {
-		  successRedirect: '/',
-		  failureRedirect: '/login' 
-		})(req, res, next);
-	},
-	// GET Logout
-	async getLogout(req, res, next) {
-	  await req.logout();
-	  res.redirect('/');
-	}
 }
